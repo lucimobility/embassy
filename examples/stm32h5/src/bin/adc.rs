3acda4ef593bed2c0c07894c34b7b905fdffa627
Kevin Lannen <kevin@luci.com>
2026-06-08 13:47:43 -0600

    Bunch of changes to the ADC example, most need dropped

--- examples/stm32h5/src/bin/adc.rs
+++ examples/stm32h5/src/bin/adc.rs
@@ -2,11 +2,19 @@
 #![no_main]
 
 use defmt::*;
+use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_stm32::Config;
-use embassy_stm32::adc::{Adc, SampleTime};
-use embassy_time::Timer;
-use {defmt_rtt as _, panic_probe as _};
+use embassy_stm32::adc::{Adc, AdcConfig, SampleTime};
+use embassy_stm32::dac::{DacChannel, Value};
+use embassy_stm32::gpio::Level::Low;
+use embassy_stm32::gpio::Output;
+use embassy_stm32::gpio::Speed::High;
+use embassy_stm32::pac::adccommon::vals::Presc;
+use embassy_stm32::pac::vrefbuf::vals::{Hiz, Vrs};
+use embassy_stm32::vrefbuf::VoltageReferenceBuffer;
+use embassy_time::{Instant, Timer};
+use panic_probe as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -43,14 +51,33 @@ async fn main(_spawner: Spawner) {
 
     info!("Hello World!");
 
-    let mut adc = Adc::new(p.ADC1);
+    let _vref = VoltageReferenceBuffer::new(p.VREFBUF, Vrs::VREF0, Hiz::CONNECTED);
+
+    let mut dac = DacChannel::new_blocking(p.DAC1, p.PA4);
+
+    let mut adc_config = AdcConfig::default();
+
+    adc_config.prescaler = Some(Presc::DIV256);
+
+    let mut adc = Adc::new_with_config(p.ADC1, adc_config);
+
+    dac.set(Value::Bit12Right(3070));
 
     let mut vrefint_channel = adc.enable_vrefint();
 
+    let mut gpio = Output::new(p.PC6, Low, High);
+
     loop {
-        let vrefint = adc.blocking_read(&mut vrefint_channel, SampleTime::CYCLES24_5);
+        let start = Instant::now();
+        gpio.set_high();
+        // 17us with no divider
+        let vrefint = adc.blocking_read(&mut vrefint_channel, SampleTime::CYCLES640_5);
+        let measured = adc.blocking_read(&mut p.PC0, SampleTime::CYCLES640_5);
+        gpio.set_low();
+        let stop = Instant::now();
+
+        info!("ADC reads took {} ns", (stop - start).as_nanos());
         info!("vrefint: {}", vrefint);
-        let measured = adc.blocking_read(&mut p.PA0, SampleTime::CYCLES24_5);
         info!("measured: {}", measured);
         Timer::after_millis(500).await;
     }
