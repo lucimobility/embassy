b8173dc2363bf5442805024643a27de099e24069
Kevin Lannen <kevin@luci.com>
2026-06-08 13:47:27 -0600

    STM32: Add config option to v3 ADC to set the ADC clock prescaler and configure the prescaler before calibrating
    Also updates the H5 example to use the prescaler to test

--- examples/stm32h5/src/bin/adc.rs
+++ examples/stm32h5/src/bin/adc.rs
@@ -2,11 +2,13 @@
 #![no_main]
 
 use defmt::*;
+use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_stm32::Config;
-use embassy_stm32::adc::{Adc, SampleTime};
+use embassy_stm32::adc::{Adc, AdcConfig, SampleTime};
+use embassy_stm32::pac::adccommon::vals::Presc;
 use embassy_time::Timer;
-use {defmt_rtt as _, panic_probe as _};
+use panic_probe as _;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -43,12 +45,15 @@ async fn main(_spawner: Spawner) {
 
     info!("Hello World!");
 
-    let mut adc = Adc::new(p.ADC1);
+    let mut adc_config = AdcConfig::default();
+    adc_config.prescaler = Some(Presc::DIV256);
+    let mut adc = Adc::new_with_config(p.ADC1, adc_config);
 
     let mut vrefint_channel = adc.enable_vrefint();
 
     loop {
         let vrefint = adc.blocking_read(&mut vrefint_channel, SampleTime::CYCLES24_5);
+
         info!("vrefint: {}", vrefint);
         let measured = adc.blocking_read(&mut p.PA0, SampleTime::CYCLES24_5);
         info!("measured: {}", measured);
